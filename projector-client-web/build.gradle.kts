8683a7aa3a239b2c12ce28894ef96769eb55209b
SerVB <servbul@yandex.ru>
2021-12-16 18:18:34 +0300

    Switch to JS IR

--- projector-client-web/build.gradle.kts
+++ projector-client-web/build.gradle.kts
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalDceDsl
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalDistributionDsl
 
 plugins {
   kotlin("js")
@@ -61,12 +61,13 @@ dependencies {
 }
 
 kotlin {
-  js {
+  js(IR) {
     browser {
-      @OptIn(ExperimentalDceDsl::class)
-      dceTask {
-        keep.add("projector-client-projector-client-web.org.jetbrains.projector.client.web.onLoad")
+      @OptIn(ExperimentalDistributionDsl::class)
+      distribution {
+        directory = project.file("build/distributions")
       }
     }
+    binaries.executable()
   }
 }
