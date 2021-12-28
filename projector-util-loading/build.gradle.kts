1c61796094285b5f7adf0c14deb8153ae9d5e213
Art Pinch <leonardo906@mail.ru>
2021-12-28 15:50:30 +0300

    Fix jacoco

--- projector-util-loading/build.gradle.kts
+++ projector-util-loading/build.gradle.kts
@@ -15,7 +15,7 @@ publishToSpace("java")
 val intellijPlatformVersion: String by project
 
 dependencies {
-  api(project(":projector-common"))
+  compileOnly(project(":projector-common"))
   api(project(":projector-util-logging"))
 
   compileOnly("com.jetbrains.intellij.platform:bootstrap:$intellijPlatformVersion")
